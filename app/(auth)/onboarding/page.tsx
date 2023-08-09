0665eb6ac9d457c38d98c217d0a7509c34ce4d69
AlexDjangoX <alexmonk17@gmail.com>
2023-08-09 18:55:33 +0200

    capatalise Page.jsx

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { currentUser } from '@clerk/nextjs';
 
-const page = async () => {
+const Page = async () => {
   const user = await currentUser();
 
   if (!user) return null;
@@ -10,4 +10,4 @@ const page = async () => {
   return <h1>Onboarding</h1>;
 };
 
-export default page;
+export default Page;
