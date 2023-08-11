6672473a0585cffa1c1c3eb524a07d1af442c085
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-08-11 12:05:12 +0300

    issues addressed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
-import AdvertOne from "@/components/AdvertOne";
-import AdvertTwo from "@/components/AdvertTwo";
+import Advert from "@/components/Advert";
 
 export default function Home() {
   return (
@@ -8,8 +7,19 @@ export default function Home() {
       <UserButton afterSignOutUrl="/" />
       <section className="flex w-full max-w-7xl">
         <div className="flex w-full flex-col gap-8 lg:flex-row">
-          <AdvertOne />
-          <AdvertTwo />
+          <Advert
+            title="The Best Platform for Car Rental"
+            description="Ease of doing a car rental safely and reliably. Of course at a low
+          price."
+            imageSrc="/pngs/advertWhiteCar.png"
+            backgroundStyle="white_car_ad"
+          />
+          <Advert
+            title="Easy way to rent a car at a low price"
+            description="Providing cheap car rental services and safe and comfortable facilities."
+            imageSrc="/pngs/advertSilverCar.png"
+            backgroundStyle="black_car_ad"
+          />
         </div>
       </section>
     </div>
---
+++ components/Advert.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+
+interface AdvertProps {
+  title: String;
+  description: String;
+  imageSrc: String;
+  backgroundStyle: String;
+}
+
+const Advert: React.FC = ({
+  title,
+  description,
+  imageSrc,
+  backgroundStyle,
+}: AdvertProps) => {
+  return (
+    <div
+      className={`${backgroundStyle} flex h-60 w-full flex-col justify-between rounded-xl px-6 pb-3 pt-6 sm:h-[22.5rem]`}
+    >
+      <div className="flex flex-col">
+        <p className="w-full text-white sm:text-3xl lg:w-1/2">{title}</p>
+        <p className="mt-4 w-full text-xs text-white sm:text-base lg:w-1/2">
+          {description}
+        </p>
+      </div>
+      <Image
+        src={imageSrc}
+        alt="car picture"
+        width={192}
+        height={56}
+        className="ml-6 h-14 w-48 self-center sm:h-[7.25rem] sm:w-[25.5rem]"
+      />
+    </div>
+  );
+};
+
+export default Advert;
