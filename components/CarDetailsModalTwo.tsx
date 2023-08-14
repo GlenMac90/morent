5b23957d5147ed25ffeef3f610bf23720789e0e7
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-08-15 01:32:21 +0300

    improved modal css

--- components/CarDetailsModalTwo.tsx
+++ components/CarDetailsModalTwo.tsx
@@ -11,7 +11,7 @@ const CarDetailsModalTwo: React.FC<CarDetailsModalTwoProps> = ({
   setShowModal,
 }) => {
   return (
-    <section className="w-full flex-1 flex-col self-center py-6 xs:min-w-[21rem] sm:w-[31.25rem] md:p-6">
+    <section className="w-full flex-1 flex-col self-center py-6 xs:min-w-[21rem] sm:w-[31.25rem] sm:p-6">
       <div className="flex justify-between">
         <div className="flex flex-col">
           <p className="text-xl font-semibold text-gray900">
@@ -25,7 +25,7 @@ const CarDetailsModalTwo: React.FC<CarDetailsModalTwoProps> = ({
           src={cross}
           alt="close modal"
           onClick={() => setShowModal(false)}
-          className="flex h-6 w-6 -translate-y-6 cursor-pointer self-start md:h-8 md:w-8 md:-translate-y-0"
+          className="flex h-6 w-6 -translate-y-6 cursor-pointer self-start sm:h-8 sm:w-8 sm:-translate-y-0"
         />
       </div>
       <p className="mt-10 text-lg font-bold text-blue500">PICKUP INFO</p>
@@ -34,7 +34,7 @@ const CarDetailsModalTwo: React.FC<CarDetailsModalTwoProps> = ({
         className="mt-3 w-full rounded-lg bg-white200 py-3.5 pl-6 text-sm"
         placeholder="Location Address"
       />
-      <div className="mt-6 flex flex-col md:flex-row md:gap-2.5">
+      <div className="mt-6 flex flex-col sm:gap-2.5 md:flex-row">
         <div className="flex w-full flex-col">
           <div className="flex">
             <Image src={calendar} alt="calender-icon" height={16} width={16} />
@@ -46,7 +46,7 @@ const CarDetailsModalTwo: React.FC<CarDetailsModalTwoProps> = ({
           />
         </div>
         <div className="flex w-full flex-col">
-          <div className="mt-2 flex md:mt-0">
+          <div className="mt-2 flex sm:mt-0">
             <Image src={calendar} alt="calender-icon" height={16} width={16} />
             <p className="ml-1.5">Availability To</p>
           </div>
