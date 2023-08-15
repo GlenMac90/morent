45e3fe1a72c8d4767c3b8e387b8a02ebffb07dc6
AlexDjangoX <alexmonk17@gmail.com>
2023-08-15 09:41:56 +0200

    lib/validations/car.ts updated - kept most options optional for development

--- lib/validations/car.ts
+++ lib/validations/car.ts
@@ -1,13 +1,15 @@
 import * as z from 'zod';
 
 export const CarValidation = z.object({
-  carTitle: z.any(),
-  carType: z.any(),
-  rentPrice: z.any(),
-  capacity: z.any(),
-  transmission: z.any(),
-  location: z.any(),
-  fuelCapacity: z.any(),
-  shortDescription: z.any(),
-  carImageMain: z.any(),
+  carTitle: z.string().min(3, 'Title should be at least 3 characters long'),
+  carType: z.string().min(3, 'Type should be at least 3 characters long'),
+  rentPrice: z.string().optional(),
+  capacity: z.number().optional(),
+  transmission: z.string().optional(),
+  location: z.string().optional(),
+  fuelCapacity: z.number().optional(),
+  shortDescription: z.string().optional(),
+  carImageMain: z.string().optional(),
+  path: z.string().optional(),
+  datesBooked: z.array(z.string()).optional(),
 });
