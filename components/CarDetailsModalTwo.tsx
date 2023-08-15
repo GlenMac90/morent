10ef44ea206e27d2918f0cd5ae3c97bc7242f9a2
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-08-15 17:41:23 +0300

    minor changes

--- components/CarDetailsModalTwo.tsx
+++ components/CarDetailsModalTwo.tsx
@@ -11,7 +11,7 @@ const CarDetailsModalTwo: React.FC<CarDetailsModalTwoProps> = ({
   setShowModal,
 }) => {
   return (
-    <section className="w-full flex-1 flex-col self-center py-6 xs:min-w-[21rem] sm:w-[31.25rem] sm:p-6">
+    <section className="w-full flex-1 flex-col self-center py-6 xs:min-w-[22rem] sm:w-[31.25rem] sm:p-6">
       <div className="flex justify-between">
         <div className="flex flex-col">
           <p className="text-xl font-semibold text-gray900">
@@ -34,7 +34,7 @@ const CarDetailsModalTwo: React.FC<CarDetailsModalTwoProps> = ({
         className="mt-3 w-full rounded-lg bg-white200 py-3.5 pl-6 text-sm"
         placeholder="Location Address"
       />
-      <div className="mt-6 flex flex-col sm:gap-2.5 md:flex-row">
+      <div className="mt-6 flex flex-col sm:flex-row sm:gap-2.5">
         <div className="flex w-full flex-col">
           <div className="flex">
             <Image src={calendar} alt="calender-icon" height={16} width={16} />
