fdf985840a7c1a2c236447d3a9413225b94bdc21
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-08-18 02:07:46 +0300

    improved modal css

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -10,16 +10,32 @@ const Loader = () => {
     <div className="z-10 flex h-screen w-screen flex-col items-center justify-center bg-white200">
       <div className="flex w-full items-center justify-center">
         <div className="z-20 h-full w-full bg-white200"></div>
-        <div className="mt-40 flex flex-col overflow-hidden rounded-t-full bg-white px-80 pt-72">
+        <div className="sky_animation mt-40 flex flex-col overflow-hidden rounded-t-full px-80 pt-72">
+          <div className="sun_moon absolute"></div>
+          <div className="star_container absolute -translate-x-24 -translate-y-52">
+            {[0, 1, 2, 3, 4].map((star) => (
+              <div key={star} className="shooting_star"></div>
+            ))}
+          </div>
+          <div className="absolute z-10 translate-x-[-4.5rem] translate-y-[2rem]">
+            {[0, 1, 2, 3, 4, 5, 6, 7].map((puff) => (
+              <div key={puff} className="smoke_list z-10"></div>
+            ))}
+          </div>
+          <div className="cloud_container absolute -translate-y-40">
+            {[0, 1, 2].map((cloud) => (
+              <div key={cloud} className="cloud absolute"></div>
+            ))}
+          </div>
           <Image
             src={advertWhiteCar}
             alt="car image"
-            height={60}
-            width={200}
-            className="z-20 min-w-[200px] self-center"
+            height={75}
+            width={250}
+            className="car_bounce z-20 min-w-[250px] self-center"
           />
           <motion.div
-            className="fixed bottom-[2rem] flex items-center self-center"
+            className="fixed top-[18.5rem] flex items-center self-center"
             animate={{ rotate: -360 }}
             transition={{
               duration: 8,
