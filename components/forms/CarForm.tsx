0cbfb76e38ff686339ebede84b0a5700b4f474c7
AlexDjangoX <alexmonk17@gmail.com>
2023-08-21 16:50:37 +0200

    extracted CarFormHeader in CarForm.jsx

--- components/forms/CarForm.tsx
+++ components/forms/CarForm.tsx
@@ -235,38 +235,12 @@ const CarForm: React.FC<Props> = ({ userId, car }) => {
           <div className="text-green-500">Car registered successfully!</div>
         )}
 
-        <div className="flex w-full flex-col justify-start">
-          {pathname === '/cars/new' ? (
-            <h1 className="text-xl font-semibold ">Add a Car for Rent</h1>
-          ) : (
-            <div className="flex w-full items-center justify-between">
-              <h1 className="text-xl font-semibold ">Edit Car Details</h1>
-              <Image
-                src={car?.carImageMain || ''}
-                width={100}
-                height={50}
-                alt="Car Image"
-              />
-            </div>
-          )}
+        <CarFormHeader
+          pathname={pathname}
+          car={car || undefined}
+          imagePreviews={imagePreviews}
+        />
 
-          <p className="mt-2.5 text-sm text-gray400">
-            Please enter your car info
-          </p>
-          <div className="mt-4 flex space-x-4">
-            {imagePreviews.map((preview, index) => (
-              <div key={index} className="relative h-24 w-24">
-                <Image
-                  src={preview}
-                  alt="Preview"
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-            ))}
-          </div>
-          <h3 className="mt-8 text-lg font-bold text-blue500">CAR INFO</h3>
-        </div>
         <div className="flex w-full flex-col gap-8 md:flex-row ">
           <InputController
             control={form.control}
@@ -356,6 +330,46 @@ const CarForm: React.FC<Props> = ({ userId, car }) => {
 };
 
 export default CarForm;
+
+type CarFormHeaderProps = {
+  pathname: string;
+  car?: {
+    carImageMain?: string;
+  };
+  imagePreviews: string[];
+};
+
+const CarFormHeader: React.FC<CarFormHeaderProps> = ({
+  pathname,
+  car,
+  imagePreviews,
+}) => (
+  <div className="flex w-full flex-col justify-start">
+    {pathname === '/cars/new' ? (
+      <h1 className="text-xl font-semibold ">Add a Car for Rent</h1>
+    ) : (
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-xl font-semibold ">Edit Car Details</h1>
+        <Image
+          src={car?.carImageMain || ''}
+          width={100}
+          height={50}
+          alt="Car Image"
+        />
+      </div>
+    )}
+
+    <p className="mt-2.5 text-sm text-gray400">Please enter your car info</p>
+    <div className="mt-4 flex space-x-4">
+      {imagePreviews.map((preview, index) => (
+        <div key={index} className="relative h-24 w-24">
+          <Image src={preview} alt="Preview" layout="fill" objectFit="cover" />
+        </div>
+      ))}
+    </div>
+    <h3 className="mt-8 text-lg font-bold text-blue500">CAR INFO</h3>
+  </div>
+);
 
 type FormData = {
   carTitle: string;
