5ef9c8fe1e45d0e08c796f1f3e2b7627a1f0a539
AlexDjangoX <alexmonk17@gmail.com>
2023-08-22 12:40:03 +0200

    extracted CarFormHeader from CarForm.tsx

--- components/forms/CarForm.tsx
+++ components/forms/CarForm.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState } from 'react';
-import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
@@ -14,7 +13,7 @@ import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing';
 import { createCar, deleteCar, editCar } from '@/lib/actions/car.actions';
 import DragDrop from './DragDrop';
-import { Props, FileWithPreview, CarFormHeaderProps } from '@/lib/interfaces';
+import { Props, FileWithPreview } from '@/lib/interfaces';
 
 import {
   carTypes,
@@ -27,6 +26,7 @@ import Location from '../Location';
 import SelectInput from './components/SelectInput';
 import InputController from './components/InputController';
 import CarFormButtons from './components/CarFormButtons';
+import CarFormHeader from './components/CarFormHeader';
 
 const CarForm: React.FC<Props> = ({ userId, car }) => {
   const { startUpload } = useUploadThing('media');
@@ -327,35 +327,3 @@ const CarForm: React.FC<Props> = ({ userId, car }) => {
 };
 
 export default CarForm;
-
-const CarFormHeader: React.FC<CarFormHeaderProps> = ({
-  pathname,
-  car,
-  imagePreviews,
-}) => (
-  <div className="flex w-full flex-col justify-start">
-    {pathname === '/cars/new' ? (
-      <h1 className="text-xl font-semibold ">Add a Car for Rent</h1>
-    ) : (
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-xl font-semibold ">Edit Car Details</h1>
-        <Image
-          src={car?.carImageMain || ''}
-          width={100}
-          height={50}
-          alt="Car Image"
-        />
-      </div>
-    )}
-
-    <p className="mt-2.5 text-sm text-gray400">Please enter your car info</p>
-    <div className="mt-4 flex space-x-4">
-      {imagePreviews.map((preview, index) => (
-        <div key={index} className="relative h-24 w-24">
-          <Image src={preview} alt="Preview" layout="fill" objectFit="cover" />
-        </div>
-      ))}
-    </div>
-    <h3 className="mt-8 text-lg font-bold text-blue500">CAR INFO</h3>
-  </div>
-);
