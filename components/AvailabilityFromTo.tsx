42547c5f683fbe6b8c310a9eac1b6174f0f7ab22
DevTaehong <minth1123@icloud.com>
2023-08-22 16:22:02 -0300

    Refactor: Make AvailabilityFromTo component

---
+++ components/AvailabilityFromTo.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import { useState } from "react";
+import { format, addDays } from "date-fns";
+import Image from "next/image";
+import { DateRange } from "react-day-picker";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import { cn } from "@/lib/utils";
+import { arrowDown, calendar, darkModeArrowDown } from "@/public/svg-icons";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+
+const AvailabilityFromTo = () => {
+  const today = new Date();
+  const twoDaysFromNow = addDays(today, 2);
+
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: twoDaysFromNow,
+    to: addDays(twoDaysFromNow, 3),
+  });
+  return (
+    <>
+      <div className="flex flex-row gap-3 xl:grow xl:gap-4">
+        <Popover>
+          <div className={`flex w-full flex-col gap-3.5`}>
+            <div className="flex flex-row">
+              <div className="flex flex-row items-center gap-[0.38rem]">
+                <Image src={calendar} width={14} height={14} alt="calendar" />
+                <Label htmlFor="availabilityFrom">Availability from</Label>
+              </div>
+            </div>
+            <PopoverTrigger asChild id="availabilityFrom">
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "bg-white200 dark:bg-gray800 w-full h-[2.875rem] sm:h-[3.5rem] justify-between border-0 text-left font-normal py-[0.69rem] px-[0.62rem] xl:pl-[1.13rem] xl:h-14",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                {date?.from ? (
+                  format(date.from, "LLL dd, y")
+                ) : (
+                  <>
+                    <span className="text-[0.625rem] font-normal leading-5 text-gray-400">
+                      Select your date
+                    </span>
+                    <Image
+                      className="dark:hidden"
+                      src={arrowDown}
+                      width="12"
+                      height="12"
+                      alt="Arrow Down"
+                    />
+                    <Image
+                      className="hidden dark:block"
+                      src={darkModeArrowDown}
+                      width="12"
+                      height="12"
+                      alt="Arrow Down"
+                    />
+                  </>
+                )}
+              </Button>
+            </PopoverTrigger>
+          </div>
+          <PopoverContent className="w-auto p-0">
+            <Calendar
+              mode={"range"}
+              selected={date}
+              onSelect={setDate}
+              numberOfMonths={2}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+
+      <div className="flex flex-row gap-3 xl:grow xl:gap-4">
+        <Popover>
+          <div className={`flex w-full flex-col gap-3.5`}>
+            <div className="flex flex-row">
+              <div className="flex flex-row items-center gap-[0.38rem]">
+                <Image src={calendar} width={14} height={14} alt="calender" />
+                <Label htmlFor="availabilityTo">Availability to</Label>
+              </div>
+            </div>
+            <PopoverTrigger asChild id="availabilityTo">
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "bg-white200 w-full dark:bg-gray800 h-[2.875rem] sm:h-[3.5rem] justify-between border-0 text-left font-normal py-[0.69rem] px-[0.62rem] xl:pl-[1.13rem] xl:h-14",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                {date?.to ? (
+                  format(date.to, "LLL dd, y")
+                ) : (
+                  <>
+                    <span className="text-[0.625rem] font-normal leading-5 text-gray-400">
+                      Select your date
+                    </span>
+                    <Image
+                      className="dark:hidden"
+                      src={arrowDown}
+                      width="12"
+                      height="12"
+                      alt="Arrow Down"
+                    />
+                    <Image
+                      className="hidden dark:block"
+                      src={darkModeArrowDown}
+                      width="12"
+                      height="12"
+                      alt="Arrow Down"
+                    />
+                  </>
+                )}
+              </Button>
+            </PopoverTrigger>
+          </div>
+          <PopoverContent className="w-auto p-0">
+            <Calendar
+              mode={"range"}
+              selected={date}
+              onSelect={setDate}
+              numberOfMonths={2}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+    </>
+  );
+};
+
+export default AvailabilityFromTo;
