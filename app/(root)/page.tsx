2bee919abfc7c9dbff90e9ce886b5a105f6051a2
AlexDjangoX <alexmonk17@gmail.com>
2023-08-31 11:52:43 +0200

    FIX: deployment errors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,8 @@
-import { motion } from "framer-motion";
-
-import CarCard from "@/components/carCardComponents/CarCard";
-import PickUpDropOffCard from "@/components/PickUpDropOffCard";
-import AdvertsDisplay from "@/components/advertsComponents/AdvertsDisplay";
-import { dummyData } from "@/utils/dummyCarData";
-import { fetchAllCars } from "@/lib/actions/car.actions";
+import CarCard from '@/components/carCardComponents/CarCard';
+import PickUpDropOffCard from '@/components/PickUpDropOffCard';
+import AdvertsDisplay from '@/components/advertsComponents/AdvertsDisplay';
+import { dummyData } from '@/utils/dummyCarData';
+import { fetchAllCars } from '@/lib/actions/car.actions';
 
 const Home = async () => {
   const data = await fetchAllCars();
@@ -36,7 +34,7 @@ const Home = async () => {
               carData={dummyData}
               key={card}
               isPopularCar={true}
-              id={"123"}
+              id={'123'}
             />
           ))}
           {/* Change id to match id of card once live data is passed */}
@@ -47,7 +45,7 @@ const Home = async () => {
         <div className="mt-5 flex w-full flex-col items-center justify-center gap-5 px-5 sm:grid sm:grid-cols-2 sm:gap-6 lg:grid-cols-3 xl:grid-cols-4">
           {/* Change id to match id of card once live data is passed */}
           {[0, 1, 2, 3, 4, 5, 6, 7].map((card) => (
-            <CarCard carData={dummyData} key={card} id={"123"} />
+            <CarCard carData={dummyData} key={card} id={'123'} />
           ))}
           {/* Change id to match id of card once live data is passed */}
         </div>
