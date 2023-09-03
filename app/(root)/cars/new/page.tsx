09846e3b74f2a8bfd30c60e6a454e34f510205e9
AlexDjangoX <alexmonk17@gmail.com>
2023-09-03 18:17:59 +0200

    FIX: type error

--- app/(root)/cars/new/page.tsx
+++ app/(root)/cars/new/page.tsx
@@ -1,8 +1,8 @@
-import { currentUser } from "@clerk/nextjs";
+import { currentUser } from '@clerk/nextjs';
 
-import CarForm from "@/components/forms/CarForm";
-import { userFromDB } from "@/lib/actions/user.actions";
-import { objectToStringId } from "@/utils/utility.serverFunctions";
+import CarForm from '@/components/forms/CarForm';
+import { userFromDB } from '@/lib/actions/user.actions';
+import { objectToStringId } from '@/utils/utility.serverFunctions';
 
 const Page = async () => {
   let user;
@@ -20,7 +20,7 @@ const Page = async () => {
   return (
     <div className="flex w-screen items-center justify-center bg-white200 dark:bg-gray900">
       <div className="mb-12 mt-[7.6rem] flex w-screen max-w-4xl items-center justify-center px-5 md:mt-[9.4rem]">
-        <CarForm userId={userIdString} />
+        <CarForm userId={userIdString as string} />
       </div>
     </div>
   );
